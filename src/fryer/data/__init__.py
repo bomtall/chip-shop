1e3b5c867004c4364b8ac5c202676768687c447a
bomtall <bomtall1@hotmail.com>
2024-12-25 15:55:12 +0000

    second fix

--- src/fryer/data/__init__.py
+++ src/fryer/data/__init__.py
@@ -8,9 +8,9 @@ from fryer.data import (
 
 
 __all__ = [
+    "monitor",
     "os_codepoint_postcodes",
     "uk_gov_compare_school_performance",
     "uk_gov_hm_land_registry_price_paid",
     "uk_police_crime_data",
-    "monitor",
 ]
