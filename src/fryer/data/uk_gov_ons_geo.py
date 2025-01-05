768b091ce4018ba09420454574df4534a38a9bbb
realmordor <realmordor@github.com>
2025-01-05 01:28:26 +0000

    add data

--- src/fryer/data/uk_gov_ons_geo.py
+++ src/fryer/data/uk_gov_ons_geo.py
@@ -225,6 +225,85 @@ class BoundariesType(Enum):
         url_about="https://geoportal.statistics.gov.uk/datasets/ons::local-administrative-units-level-2-january-2015-boundaries-ew-bfc-1/about",
     )
 
+    # Healthcare
+    CAL_JUL_2023_EN_BFC = BoundariesData(
+        name_short="CAL23",
+        url_key="Cancer_Alliances_July_2023_Boundaries_EN_BFC",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::cancer-alliances-july-2023-boundaries-en-bfc-2/about",
+    )
+    CCG_APR_2021_EN_BFC = BoundariesData(
+        name_short="CCG21",
+        url_key="Clinical_Commissioning_Groups_April_2021_EN_BFC_2022",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::clinical-commissioning-groups-april-2021-en-bfc-1/about",
+    )
+    CIS_DEC_2020_UK_BFC = BoundariesData(
+        name_short="CIS20",
+        url_key="Covid_Infection_Survey_Dec_2020_UK_BFC",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::covid-infection-survey-december-2020-uk-bfc/about",
+    )
+    ICB_APR_2023_EN_BFC = BoundariesData(
+        name_short="ICB23",
+        url_key="Integrated_Care_Boards_April_2023_EN_BFC",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::integrated-care-boards-april-2023-en-bfc-2/about",
+    )
+    SICBL_APR_2023_EN_BFC = BoundariesData(
+        name_short="SICBL23",
+        url_key="Sub_Integrated_Care_Board_Locations_April_2023_EN_BFC",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::sub-integrated-care-board-locations-april-2023-en-bfc-2/about",
+    )
+    NHSER_JAN_2024_EN_BFC = BoundariesData(
+        name_short="NHSER24",
+        url_key="NHS_England_Regions_January_2024_EN_BFC",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::nhs-england-regions-january-2024-en-bfc-2/about",
+    )
+    NHS_England_Region_Local_Office_April_2018_EN_BFC = BoundariesData(
+        name_short="nhsrlo18",
+        url_key="NHS_England_Region_Local_Office_April_2018_EN_BFC_2022",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::nhs-england-region-local-office-april-2018-en-bfc/about",
+    )
+    STP_APR_2021_EN_BFC = BoundariesData(
+        name_short="STP21",
+        url_key="Sustainability_and_Transformation_Partnerships_April_2021_EN_BFC_2022",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::sustainability-and-transformation-partnerships-april-2021-en-bfc-3/about",
+    )
+
+    # Other
+    CSP_DEC_2023_EW_BFC = BoundariesData(
+        name_short="CSP23",
+        url_key="Community_Safety_Partnerships_December_2023_Boundaries_EW_BFC",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::community-safety-partnerships-december-2023-boundaries-ew-bfc-2/about",
+    )
+    FRA_DEC_2023_EW_BFC = BoundariesData(
+        name_short="FRA23",
+        url_key="Fire_and_Rescue_Authorities_December_2023_EW_BFC",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::fire-and-rescue-authorities-december-2023-ew-bfc-2/about",
+    )
+    LEP_DEC_2022_EN_BFC_V2 = BoundariesData(
+        name_short="LEP22",
+        url_key="LEP_DEC_2022_EN_BFC_V2",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::local-enterprise-partnerships-december-2022-en-bfc-v2/about",
+    )
+    LSIP_AUG_2023_EN_BFC = BoundariesData(
+        name_short="LSIP23",
+        url_key="Local_Skills_Improvement_Plan_Areas_August_2023_Boundaries_EN_BFC",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::local-skills-improvement-plan-areas-august-2023-boundaries-en-bfc-2/about",
+    )
+    NPARK_DEC_2022_GB_BFC_V2 = BoundariesData(
+        name_short="NPARK22",
+        url_key="National_Parks_December_2022_Boundaries_GB_BFC",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::national-parks-december-2022-boundaries-gb-bfc-v2/about",
+    )
+    PFA_DEC_2023_EW_BFC = BoundariesData(
+        name_short="PFA23",
+        url_key="Police_Force_Areas_December_2023_EW_BFC",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::police-force-areas-december-2023-ew-bfc-2/about",
+    )
+    REGD_DEC_2021_EW_BFC_v2 = BoundariesData(
+        name_short="REGD21",
+        url_key="Registration_Districts_December_2021_Boundaries_EW_BFC_v2",
+        url_about="https://geoportal.statistics.gov.uk/datasets/ons::registration-districts-december-2021-boundaries-ew-bfc-v2-1/about",
+    )
+
     @property
     def data(self):
         return self.value
